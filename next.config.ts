48fec8382a5b70efbd1614351ff4d97c33c06a47
kimgho <kgh67811@naver.com>
2024-12-28 03:13:42 +0900

    build: add Dependencies
    - add Dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,22 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
 
 const isProduction = process.env.NODE_ENV === "production";
+const analyzing = process.env.ANALYZE === "true";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  trailingSlash: true,
   basePath: isProduction ? "/glog" : "",
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: analyzing,
+  openAnalyzer: true,
+});
+
+export default withBundleAnalyzer(nextConfig);
