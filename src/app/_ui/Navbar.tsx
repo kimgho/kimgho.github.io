b841c2eebf680764ae659c647821c1f7e6187f2b
kimgho <kgh67811@naver.com>
2024-12-30 02:20:19 +0900

    style(Navbar): Modify Transparency

--- src/app/_ui/Navbar.tsx
+++ src/app/_ui/Navbar.tsx
@@ -7,7 +7,7 @@ import { ModeToggle } from "@/components/ModeToggle";
 
 const Navbar = () => {
   return (
-    <nav className="fixed py-4 inset-x-0 top-0 z-50 border-b border-border/40 bg-background/95 backdrop-blur-sm supports-[backdrop-filter]:bg-background/60">
+    <nav className="fixed py-4 inset-x-0 top-0 z-50 border-b border-border/40 bg-background/95 backdrop-blur-0 supports-[backdrop-filter]:bg-background/60">
       <div className="relative px-5 max-w-7xl mx-auto">
         <div className="flex justify-between items-center">
           <div className="font-bold lg:text-xl lg:font-semibold ">
