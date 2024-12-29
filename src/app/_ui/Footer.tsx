e48d414f774f028cc3a18fd7dea952ad547e8bff
kimgho <kgh67811@naver.com>
2024-12-30 02:20:42 +0900

    style(Footer): change style

--- src/app/_ui/Footer.tsx
+++ src/app/_ui/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-8 text-center">
+    <footer className="bg-white border-t border-border/90 text-black p-8 text-center">
       <p>&copy; 2024 Glog</p>
     </footer>
   );
