8e8f789ec252415629493c69771e6b49e30720bb
kimgho <kgh67811@knu.ac.kr>
2025-12-24 20:02:47 +0900

    feat(lint): a11y eslint추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,46 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+import tseslint from "typescript-eslint";
+import next from "@next/eslint-plugin-next";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import { globalIgnores } from "eslint/config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig = [
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "@next/next": next,
+      "jsx-a11y": jsxA11y,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+      ...next.configs["core-web-vitals"].rules,
+      ...jsxA11y.flatConfigs.recommended.rules,
+      "jsx-a11y/alt-text": "error",
+    },
+  },
+  {
+    files: [".storybook/**"],
+    languageOptions: {
+      parserOptions: {
+        projectService: false,
+        project: null,
+      },
+    },
+  },
+  globalIgnores(["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts", ".pnp.cjs", ".pnp.loader.mjs", ".yarn/**"]),
+  ...storybook.configs["flat/recommended"],
+];
 
 export default eslintConfig;
