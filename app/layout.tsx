b5d94e6729bdfa5160f9df53f6ae64d870a704cd
kimgho <kgh67811@knu.ac.kr>
2025-12-25 02:10:28 +0900

    feat(layout): metadata 추가 및 헤더,푸터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,39 @@
+import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 
-const pretendard = localFont({
+import Footer from "@/app/components/shared/Footer";
+import Header from "@/app/components/shared/Header";
+
+import "@/app/globals.css";
+
+export const pretendard = localFont({
   src: "./fonts/Pretendard-Regular.woff2",
   variable: "--font-pretendard",
   display: "swap",
   weight: "400",
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://kimgho.github.io"),
+  title: {
+    template: "%s | 프론트엔드를 꿈꾸는 김건호의 블로그",
+    default: "프론트엔드를 꿈꾸는 김건호의 블로그",
+  },
+  description: "프론트엔드를 꿈꾸는 김건호의 블로그",
+  openGraph: {
+    title: "프론트엔드를 꿈꾸는 김건호의 블로그",
+    description: "프론트엔드를 꿈꾸는 김건호의 블로그",
+    url: "https://kimgho.github.io",
+    siteName: "프론트엔드를 꿈꾸는 김건호의 블로그",
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "프론트엔드를 꿈꾸는 김건호의 블로그",
+    description: "프론트엔드를 꿈꾸는 김건호의 블로그",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -15,7 +42,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${pretendard.variable} antialiased`}>{children}</body>
+      <body className={`${pretendard.variable} antialiased`}>
+        <div className="bg-bg-main text-white min-h-screen flex flex-col font-sans selection:bg-blue-600 selection:text-white">
+          <Header />
+          <main className="grow flex flex-col items-center w-full max-w-300 mx-auto">
+            {children}
+          </main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
