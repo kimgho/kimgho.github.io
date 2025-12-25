0cfc43fee586c64762e0c2e80eaf1103744c7d02
kimgho <kgh67811@knu.ac.kr>
2025-12-25 18:42:29 +0900

    feat(resume): 소개 페이지 추가

---
+++ app/resume/_components/index.ts
@@ -0,0 +1,5 @@
+export { ActivitySection } from "./ActivitySection";
+export { AwardSection } from "./AwardSection";
+export { EducationSection } from "./EducationSection";
+export { ResumeCard } from "./ResumeCard";
+export { IntroduceSection } from "./IntroduceSection";
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,21 @@
+import {
+  ActivitySection,
+  EducationSection,
+  AwardSection,
+  IntroduceSection,
+} from "@/app/resume/_components";
+
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
+    <div className="max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8 py-8 md:py-12">
+      <div className="lg:col-span-8 flex flex-col gap-8 lg:gap-12">
+        <div className="flex-1 space-y-12">
+          <IntroduceSection />
+          <ActivitySection />
+          <EducationSection />
+          <AwardSection />
+        </div>
+      </div>
     </div>
   );
 }
